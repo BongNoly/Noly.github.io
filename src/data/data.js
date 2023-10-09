f233694107f2405084f3173fd1962c9d27cec1d1
BongNoly <125236108+BongNoly@users.noreply.github.com>
2023-10-09 23:15:30 +0700

    Add files via upload

---
+++ src/data/data.js
@@ -0,0 +1,25 @@
+export const data = [
+  {
+    id: 1,
+    img: "https://i.postimg.cc/br3FMbxb/Motion-Graphic.jpg",
+    title: "Graphic Designer",
+    desc: "",
+    link: "",
+    git: "",
+  },
+  {
+    id: 2,
+    img: "https://i.postimg.cc/zG929qgd/microsft.jpg",
+    title: "Thecher of Computer",
+    link: "",
+    git: "",
+  },
+  {
+    id: 3,
+    img: "https://i.postimg.cc/J4gLKDRv/57214s81.jpg",
+    title: "Frontend Developer",
+    desc: "",
+    link: "",
+    git: "",
+  },
+];
